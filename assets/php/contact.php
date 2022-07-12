c7a4d2f37eb98d4aa9fbc5364f491baf9c81ddf5
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-07-12 15:53:00 +0200

    furmulaire php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,16 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
 <?php
     if (isset($_POST['message'])) {
         $entete  = 'MIME-Version: 1.0' . "\r\n";
         $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $entete .= 'From: webmaster@monsite.fr' . "\r\n";
+        $entete .= 'From: contact@vincentb.fr' . "\r\n";
         $entete .= 'Reply-to: ' . $_POST['email'];
 
-        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+        $message = '<h1>Message envoyé depuis la page Contact</h1>
+
+        <p><b>Premon : </b>' . $_POST['name'] . '<br>
         <p><b>Email : </b>' . $_POST['email'] . '<br>
+        <p><b>Sujet : </b>' . $_POST['subject'] . '<br><br>
         <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
 
-        $retour = mail('destinataire@free.fr', 'Envoi depuis page Contact', $message, $entete);
+        $retour = mail('vincent-benoit@outlook.fr', 'Envoi depuis page Contact', $message, $entete);
         if($retour)
             echo '<p>Votre message a bien été envoyé.</p>';
     }
     ?>
+</body>
+</html>
