752248005547f6703106b328200b12622f5a198d
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-08-12 14:13:37 +0200

    legend delet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,46 @@
-var ctx = document.getElementById("myChart").getContext("2d");
+var ctx = document.getElementById("myChart");
 var data = {
     labels: ["PHP", "Javascript", "Laravel", "Sass", "HTML & CSS", "Bootstrap", "Git", "Wordpress"],
     datasets: [
         {
-            label: "Relative Skill",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            label: "Compétences",
+            fill: true,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            pointBackgroundColor: 'rgb(255, 99, 132)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
             data: [60, 75, 55, 60, 100, 85, 80, 45]
-        }
-    ]
-};
-new Chart(ctx, {'type':'radar','data': data, 'options': {	
-	scaleOverlay : false,
-	scaleOverride : false,
-	scaleSteps : null,
-	scaleStepWidth : null,
-	scaleStartValue : null,
-	scaleShowLine : true,
-	scaleLineColor : "#999",
-	scaleLineWidth : 1,
-	scaleShowLabels : false,
-	scaleLabel : "<%=value%>",
-	scaleFontFamily : "'Arial'",
-	scaleFontSize : 12,	
-	scaleFontStyle : "normal",
-	scaleFontColor : "#666",
-	scaleShowLabelBackdrop : true,
-	scaleBackdropColor : "rgba(255,255,255,1)",
-	scaleBackdropPaddingY : 20,	
-	scaleBackdropPaddingX : 200,
-	angleShowLineOut : true,
-	angleLineColor : "rgba(255,255,255,0.3)",
-	angleLineWidth : 1,			
-	pointLabelFontFamily : "'Arial'",
-	pointLabelFontStyle : "normal",
-	pointLabelFontSize : 12,
-	pointLabelFontColor : "#EFEFEF",
-	pointDot : true,
-	pointDotRadius : 3,
-	pointDotStrokeWidth : 1,
-	datasetStroke : true,
-	datasetStrokeWidth : 1,
-	datasetFill : true,
-	animation : true,
-	animationSteps : 60,
-	animationEasing : "easeOutQuart",
-	onAnimationComplete : null
-	
+        }, {
+            label: 'My Second Dataset',
+            data: [10, 10, 10, 10, 10, 10, 10, 10],
+            fill: true,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgb(54, 162, 235)',
+            pointBackgroundColor: 'rgb(54, 162, 235)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(54, 162, 235)'
+          }]
+        };
+new Chart(ctx, {'type':'radar','data': data, options: {plugins: {legend: {display: false,}}} ,'options': {
+	animationSteps: 30,
+	animationEasing: "easeInOutExpo",
+	responsive: true,
+	showTooltips: true,
+	scaleOverride: true,
+    scaleSteps: 5,
+    scaleStepWidth: 100,
+    scaleStartValue: 50,
+    scaleLineColor: "rgba(200,200,200,.15)",
+    angleShowLineOut: true,
+    angleLineWidth : 1,
+    angleLineColor : "rgba(200,200,250,.15)",
+    pointLabelFontFamily : "'freight-sans-pro', Calibri, Candara, Segoe, 'Segoe UI', Optima, Arial, sans-serif",
+    pointLabelFontSize : 14,
+	pointLabelFontColor : "#99b",
+	pointDot : false,
+	datasetStrokeWidth : 1
+    
 }});
